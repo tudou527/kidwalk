52d8693ba6bd1ffc44033e733fa0d41de1691675
xiaoyun.fuxy <xiaoyun.fuxy@alibaba-inc.com>
2025-11-20 19:52:58 +0800

    feat: mock 数据

--- web/server/puppeteer.ts
+++ web/server/puppeteer.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import puppeteer from 'puppeteer';
 
 import { getLinkFromText } from '../util/common';
@@ -12,59 +13,71 @@ async function getDetail(url: string) {
       '--disable-background-networking',
     ],
   });
+  browser.setCookie({
+    name: 'web_session',
+    value: '040069b402dcad37404658bf2b3b4b13df14b4',
+    domain: '.xiaohongshu.com',
+    httpOnly: true,
+    secure: true,
+    expires: 1795173041572,
+  });
   const page = await browser.newPage();
-
-  await browser.setCookie({
-    name: 'cookie1',
-    value: '1',
-    domain: 'localhost',
-    path: '/',
-    sameParty: false,
-    expires: -1,
-    httpOnly: false,
-    secure: false,
-    sourceScheme: 'NonSecure',
-  });
-
-  await page.setRequestInterception(true);
-  page.on('request', request => {
-    if (request.isInterceptResolutionHandled()) {
-      return;
-    };
-    // 不加载图片
-    if (['image', 'stylesheet', 'font', 'media'].includes(request.resourceType())) {
-      request.abort();
-    } else {
-      request.continue();
-    }
-  });
-  page.on('response', response => {
-    // console.log(response.url());
-  });
-
+  await page.setViewport({ width: 1280, height: 800 });
   await page.goto(url);
-  await page.setViewport({ width: 1080, height: 1024 });
+  await page.waitForSelector('body');
 
-  // 在页面环境中暴露方法
-  await page.exposeFunction('getNodeInfo', async () => {
-    return document.body.innerHTML;
+  await page.waitForSelector('body');
+  await page.screenshot({ path: 'example.png' });
+
+  const pageData = await page.evaluate(async () => {
+    const initState = (window as unknown as any).__INITIAL_STATE__ || {};
+    if (!initState.note) {
+      return null;
+    }
+
+    const currentNoteId = initState.note.currentNoteId._value;
+    const noteDetailMap = initState.note.noteDetailMap;
+    const { comments, note } = noteDetailMap[currentNoteId] || {};
+
+    // 加载更多评论
+    const getMoreComments = async (cursor: string, index: number) => {
+      const params = new URLSearchParams({
+        note_id: currentNoteId,
+        cursor,
+        top_comment_id: '',
+        xsec_token: note.xsecToken,
+        image_formats: 'jpg,webp,avif',
+      });
+      const commentResponse = await fetch(`https://edith.xiaohongshu.com/api/sns/web/v2/comment/page?${params.toString()}`, {
+        method: 'get',
+        credentials: 'include'
+      }).then(res => res.json());
+
+      if (commentResponse.success) {
+        comments.list.push(...commentResponse.data.comments);
+        comments.hasMore = commentResponse.data.hasMore;
+        comments.cursor = commentResponse.data.cursor;
+
+        note.xsecToken = commentResponse.data.xsec_token;
+      }
+
+      if (comments.hasMore && index < 5) {
+        await new Promise(resolve => setTimeout(resolve, 1500));
+        await getMoreComments(comments.cursor, index + 1);
+      }
+    }
+
+    if (comments.hasMore) {
+      await getMoreComments(comments.cursor, 1);
+    }
+
+    return { comments, note };
   });
-  
-  const data = await page.evaluate(async () => {
-    return document.documentElement.innerHTML;
-    // const info = await window.getNodeInfo();
-    // // 登录判断
-    // return {
-    //   width: window.innerWidth,
-    //   height: window.innerHeight,
-    //   url: window.location.href,
-    //   info,
-    // };
-  });
-  console.log('>>>>> data: ', data);
 
   await page.close();
   await browser.close();
+
+  return pageData;
 }
 
 new Promise(async () => {
@@ -73,9 +86,13 @@ new Promise(async () => {
     复制后打开【小红书】查看笔记！
     `;
   const [link] = getLinkFromText(demoText);
+  console.log('>>>>> link: ', link);
+
   if (!link) {
     return;
   }
+  const result = await getDetail(link);
+  fs.writeFileSync('./result.json', JSON.stringify(result, null, 2));
+}).then(() => {
 
-  await getDetail(link);
-});
+})
